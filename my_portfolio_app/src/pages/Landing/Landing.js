e5ed328d65f45c606937ac0e78b719656e38c6e9
Irina Papakhina <papahinairina@gmail.com>
2023-10-09 22:28:28 +0300

    chanded icon and picture on land page

--- my_portfolio_app/src/pages/Landing/Landing.js
+++ my_portfolio_app/src/pages/Landing/Landing.js
@@ -1,9 +1,16 @@
-import { Container, Typography } from "@mui/material"
+import { useMatchMedia } from "../../shared/utils/hooks/matchMediaHook"
+import { Container, Typography, Box } from "@mui/material"
 import { headerHeight } from "../../shared/constants/componentSize"
 import { spacing } from "../../shared/constants/spacing"
 import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown"
+import Lottie from "lottie-react"
+import Animation from "../../shared/ui/animation/animationLandingPage.json"
+
+
 
 const Landing = () => {
+  const {isMobile, isTablet, isDesktop} = useMatchMedia();
+
   return (
   <Container>
     <Container
@@ -14,10 +21,12 @@ const Landing = () => {
         gridTemplateRows: "1fr 100px",
         height: `calc(100vh - ${headerHeight})`,
         justifyContent: 'center',
-        paddingTop: spacing[3]
+        paddingTop: spacing[3],
+        minWidth: isMobile ? '390px' : null || isTablet ? '767px' : null || isDesktop ? '1820px' : null,
+        maxWidth: isMobile ? '766px' : null || isTablet ? '1199px' : null || isDesktop ? '3000px' : null,
       }}>
         <Typography 
-          variant="h3"
+          variant= 'subtitle1'
           align="center"
           sx={{
             fontWeight: 'bold',          
@@ -28,6 +37,21 @@ const Landing = () => {
           sx={{
             fontSize: '50px',
           }}/>
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: '-10px',
+            left: '10px',
+            minWidth: isMobile ? '100px' : null || isTablet ? '300px' : null || isDesktop ? '500px' : null,
+            maxWidth: isMobile ? '100px' : null || isTablet ? '300px' : null || isDesktop ? '500px' : null,
+          }}>
+          <Lottie 
+            animationData={Animation}
+            loop={true}
+            autoplay={true}
+            style={{ height: '300px',width: '300px' }}
+          />
+        </Box>
     </Container>
   </Container>
   )
