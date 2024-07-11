d1d87c480af8d6bd38629c547dc04dd700feda18
Irina Papakhina <papahinairina@gmail.com>
2024-07-11 16:50:38 +0300

    fix: request weather

--- my_portfolio_app/src/shared/constants/url.js
+++ my_portfolio_app/src/shared/constants/url.js
@@ -2,4 +2,4 @@
 // export const url = process.env.API_URL ? process.env.API_URL : 'https://planner-server.up.railway.app'
 export const url = 'http://localhost:3001';
 export const urlGallery = 'http://localhost:5008';
-export const urlWeather = 'http://localhost:3002';
+export const urlWeather = 'http://localhost:3003';
